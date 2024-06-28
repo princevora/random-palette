b3799dcb31b468914936e4b1be9def8f4fdfc707
=princevora <=princevora522@gmail.com>
2024-06-28 09:15:27 +0530

    Initial commit

--- index.ts
+++ index.ts
@@ -6,9 +6,43 @@
  */
 
 function generatePalette(amount: number = 5): string {
-    const VALID_PALETTE_CHARS = "0123456789ABCDEF";
-    console.log(VALID_PALETTE_CHARS);
-    return "!";
+    
+    if(typeof amount !== "number" || amount <= 0) return "Please provide valid amount";
+
+    // VALID_PALETTE_CHARS Are the Characters that used in creating palettes
+    const VALID_PALETTE_CHARS: string = "0123456789ABCDEF";
+
+    // VALID_PALETTE_LENGTH Is the numeric length of a normal color palette
+    const PALETTE_LENGTH: number = 6;
+
+    let COLOR_PALETTE: string = "#";
+
+    // Run the loop for 6 times..
+    for (let index = 0; index < PALETTE_LENGTH; index++) {
+
+        /**
+         * First of all we are generating a random number between 0 to 1
+         * Then we are multipliying It by 16 as our VALID_PALETTE_CHARS 's length is 15 
+         * And then we are retriving a absolute / Nearest number of the previous number => [Math.random() * 15]
+         */
+        const PALETTE_INDEX = Math.round(Math.random() * 15);
+
+        /**
+         * Get the palette char for the palette index
+         */
+        const PALETTE_CHAR = VALID_PALETTE_CHARS[PALETTE_INDEX];
+
+        /**
+         * Join the palette char to the color
+         */
+
+        COLOR_PALETTE += PALETTE_CHAR;
+    }
+
+    // Return the color palette
+    return COLOR_PALETTE;
 }
+
+console.log(generatePalette());
 
 module.exports = generatePalette;
